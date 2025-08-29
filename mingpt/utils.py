009a9029e10bedf426647735616c738c485c5f05
janrzgmz <janrgomezz@gmail.com>
2025-08-29 11:17:33 +0200

    Update utils.py

--- mingpt/utils.py
+++ mingpt/utils.py
@@ -16,6 +16,11 @@ def set_seed(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
 
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+    os.environ["PYTHONHASHSEED"] = str(seed)
+
 def setup_logging(config):
     """ monotonous bookkeeping """
     work_dir = config.system.work_dir
